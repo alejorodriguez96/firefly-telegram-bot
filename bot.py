3c926fde9827341ef94ff47ccc6c886c8e4bfa2f
Alejo Rodriguez <atrodriguez@fi.uba.ar>
2022-11-07 00:53:05 -0300

    refactor: add flake8 corrections

--- bot.py
+++ bot.py
@@ -7,23 +7,33 @@ from telegram import Update
 
 from handlers.commons import init_handlers
 
-logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
-                     level=logging.INFO)
+logging.basicConfig(
+    format="%(asctime)s - %(name)s - %(levelname)s - %(message)s",
+    level=logging.INFO
+)
+
 
 def start(update: Update, context: CallbackContext):
-    context.bot.send_message(chat_id=update.effective_chat.id, text="I'm a bot, please talk to me!")
+    context.bot.send_message(
+        chat_id=update.effective_chat.id, text="I'm a bot, please talk to me!"
+    )
+
 
 def default(update: Update, context: CallbackContext):
-    context.bot.send_message(chat_id=update.effective_chat.id, text="Sorry, I didn't understand that command.")
+    context.bot.send_message(
+        chat_id=update.effective_chat.id,
+        text="Sorry, I didn't understand that command.",
+    )
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     load_dotenv()
     # Updater
-    updater = Updater(token=os.getenv('TOKEN'), use_context=True)
+    updater = Updater(token=os.getenv("TOKEN"), use_context=True)
     dispatcher = updater.dispatcher
-    
+
     # Handlers
     init_handlers(dispatcher)
-    
+
     # Strat bot
     updater.start_polling()
--- handlers/commons.py
+++ handlers/commons.py
@@ -2,14 +2,22 @@ from telegram.ext import CallbackContext, CommandHandler, Dispatcher
 from telegram.ext import MessageHandler, Filters
 from telegram import Update
 
+
 def start(update: Update, context: CallbackContext):
-    context.bot.send_message(chat_id=update.effective_chat.id, text="I'm a bot, please talk to me!")
+    context.bot.send_message(
+        chat_id=update.effective_chat.id, text="I'm a bot, please talk to me!"
+    )
+
 
 def default(update: Update, context: CallbackContext):
-    context.bot.send_message(chat_id=update.effective_chat.id, text="Sorry, I didn't understand that command.")
+    context.bot.send_message(
+        chat_id=update.effective_chat.id,
+        text="Sorry, I didn't understand that command.",
+    )
+
 
 def init_handlers(dispatcher: Dispatcher):
-    start_handler = CommandHandler('start', start)
+    start_handler = CommandHandler("start", start)
     dispatcher.add_handler(start_handler)
     default_handler = MessageHandler(Filters.all, default)
     dispatcher.add_handler(default_handler)
